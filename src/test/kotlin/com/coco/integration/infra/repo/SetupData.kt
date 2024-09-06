25a853dd1a0c9e896befa33639862f48f8b1e6b4
coco40725 <s0912571225@gmail.com>
2024-09-06 15:55:51 +0800

    test(setupdata): 調整 test 使用的資料

--- src/test/kotlin/com/coco/integration/infra/repo/SetupData.kt
+++ src/test/kotlin/com/coco/integration/infra/repo/SetupData.kt
@@ -1,6 +1,8 @@
 package com.coco.integration.infra.repo
 
 import com.coco.infra.config.MongoConfig
+import com.coco.infra.config.WebConfig
+import com.coco.infra.constant.RedisConstant
 import com.coco.infra.util.Log
 import io.quarkus.mongodb.reactive.ReactiveMongoClient
 import io.quarkus.redis.datasource.ReactiveRedisDataSource
@@ -16,7 +18,8 @@ import org.bson.Document
 class SetupData @Inject constructor(
     private val mongoClient: ReactiveMongoClient,
     private val redisDS: ReactiveRedisDataSource,
-    private val mongoConfig: MongoConfig
+    private val mongoConfig: MongoConfig,
+    private val webConfig: WebConfig
 ){
     /**
      * 初始化單元測試用的 DB
@@ -113,6 +116,28 @@ class SetupData @Inject constructor(
                 "enabled" : false,
                 "expirationDate" : ISODate("2024-08-25T12:14:43Z")
             }
+            """.trimIndent()),
+            Document.parse("""
+               {
+                "_id" : ObjectId("66c70b62907bc04fd8e9b555"),
+                "shortLink" : "${webConfig.websiteDomain()}/disable-1",
+                "userId" : "user-123",
+                "originalLink" : "https://juejin.cn/post/7187979210391027767",
+                "lastUpdateDate" : ISODate("2024-08-25T12:15:24Z"),
+                "createDate" : ISODate("2024-08-22T09:56:50Z"),
+                "enabled" : false
+            } 
+            """.trimIndent()),
+            Document.parse("""
+            {
+                "_id" : ObjectId("66c70b62907bc04fd8e9b566"),
+                "shortLink" : "${webConfig.websiteDomain()}/enable-1",
+                "userId" : "user-123",
+                "originalLink" : "https://juejin.cn/post/7187979210391027767",
+                "lastUpdateDate" : ISODate("2024-08-25T12:15:24Z"),
+                "createDate" : ISODate("2024-08-22T09:56:50Z"),
+                "enabled" : true
+            }  
             """.trimIndent())
         )).await().indefinitely()
     }
@@ -132,9 +157,14 @@ class SetupData @Inject constructor(
             .await().indefinitely()
         hashCmd.hset("https://short.coco-dev.com/fish-123", "originalLink", "https://short.coco-dev.com/fish-123/original")
             .await().indefinitely()
+        hashCmd.hset("${webConfig.websiteDomain()}/enable-1", "originalLink", "https://quarkus.io/blog/mocking/")
+            .await().indefinitely()
+        hashCmd.hset("${webConfig.websiteDomain()}/disable-1", "originalLink", "https://quarkus.io/blog/mocking/")
+            .await().indefinitely()
+
 
         val listCmd = redisDS.list(String::class.java)
-        listCmd.lpush("whiteList", "https://short.coco-dev.com/zzz1234", "https://short.coco-dev.com/aaa1234")
+        listCmd.lpush(RedisConstant.WHITE_SHORT_LINK_KEY, "https://short.coco-dev.com/zzz1234", "https://short.coco-dev.com/aaa1234")
             .await().indefinitely()
     }
 
